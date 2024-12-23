b40e53192379f7848c563877fdac19643355f124
Your Name <dixitayush550@gmail.com>
2024-12-23 12:20:09 +0530

    create a header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Projects from "./pages/Projects";
 import About from "./pages/About"
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -16,8 +18,6 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        
-        
       </Routes>
     </BrowserRouter>
   );
