9422573b3c414140ddf527f89b011de1bc24fee1
Your Name <dixitayush550@gmail.com>
2024-12-23 18:46:18 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 
 const app = express();
 
@@ -14,10 +16,13 @@ mongoose
   })
   .catch((err) => {
     console.log(err);
-  });
+  });  
 
+  app.use(express.json());
+  
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000!!");
 });
 
 app.use('/api/user', userRoutes );
+app.use('/api/auth', authRoutes );
